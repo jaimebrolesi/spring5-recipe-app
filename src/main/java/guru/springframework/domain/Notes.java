10a02ed36d7142c56b093991a9a2b032c0a4ea91
Jaime Brolesi <jaime.brolesi@rapiddo.com.br>
2018-02-20 21:26:00 -0300

    added one to one relationship

---
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -0,0 +1,31 @@
+package guru.springframework.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Lob;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Notes extends AbstractEntity {
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
